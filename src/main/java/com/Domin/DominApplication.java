feaf16e851c502fe9abbbeb48aee1282aa74cd40
Hilixx <jhonalizan@gmail.com>
2025-06-07 13:29:00 -0300

    first

---
+++ src/main/java/com/Domin/DominApplication.java
@@ -0,0 +1,13 @@
+package com.Domin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Domin/DominApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Domin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
